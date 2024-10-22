dcd5719a662d3e0445154c08aee38bbe65c66628
GTByun <byungt02@naver.com>
2024-10-22 11:02:29 +0900

    메인메뉴 완성
    1. Dictionary 완전 구현

--- UnityProject/Test/Assets/UI_Script/ItemImage.cs
+++ UnityProject/Test/Assets/UI_Script/ItemImage.cs
@@ -8,23 +8,16 @@ public class ItemImage : MonoBehaviour
     public float imageSizer;
     Image currentImage;
     RectTransform rect;
-    Sprite[] sprites;
 
     private void Awake()
     {
         currentImage = GetComponent<Image>();
         rect = GetComponent<RectTransform>();
-        GameObject[] objects = GameObject.FindGameObjectsWithTag("ItemList");
-        sprites = new Sprite[objects.Length];
-        for (int i = 0; i < sprites.Length; i++)
-        {
-            sprites[i] = objects[i].transform.GetChild(0).gameObject.GetComponent<Image>().sprite;
-        }
     }
 
-    public void ChangeItemImage(int number)
+    public void ChangeItemImage(Image image)
     {
-        Sprite sprite = sprites[sprites.Length - number - 1];
+        Sprite sprite = image.sprite;
         currentImage.sprite = sprite;
         rect.transform.localScale = new Vector3(sprite.bounds.size.x * imageSizer, sprite.bounds.size.y * imageSizer);
     }
--- UnityProject/Test/Assets/UI_Script/ListScroll.cs
+++ UnityProject/Test/Assets/UI_Script/ListScroll.cs
@@ -4,20 +4,63 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ListScroll : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class ListScroll : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    public float yUpLimit;
+    public float yDownLimit;
+    float preY;
+    float accel;
+    public float accelSlow;
+    public float dragSpeed;
+    bool naturalMove;
+    bool accelPositive;
+
+    RectTransform rect;
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (naturalMove)
+        {
+            float direction = accelPositive ? -1 : 1;
+            accel += accelSlow * direction;
+            if ((accelPositive && accel > 0) || (!accelPositive && accel < 0))
+            {
+                rect.localPosition = new Vector2(rect.localPosition.x, Mathf.Clamp(rect.localPosition.y + accel * dragSpeed, yUpLimit, yDownLimit));
+            }
+            else
+            {
+                naturalMove = false;
+            }
+        }
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("StartDrag");
+        naturalMove = false;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, Input.mousePosition, null, out Vector2 localPoint))
+        {
+            preY = localPoint.y;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Dragging");
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, Input.mousePosition, null, out Vector2 localPoint))
+        {
+            accel = localPoint.y - preY;
+            Debug.Log(accel);
+            rect.localPosition = new Vector2(rect.localPosition.x, Mathf.Clamp(rect.localPosition.y + accel * dragSpeed, yUpLimit, yDownLimit));
+            preY = localPoint.y;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("EndDrag");
-    }
+        accelPositive = accel > 0;
+        naturalMove = true;
+    }    
 }
