e07eba5ef32654326707ed96d5bb80e2793ae75f
GTByun <byungt02@naver.com>
2024-11-08 10:30:22 +0900

    24.11.08 아이템 선택 UI 제작
    1. 스테이지 아이템 UI 제작
    2. 아이템값 본게임에 넘기게 제작

--- UnityProject/Test/Assets/UI_Script/StageLoader.cs
+++ UnityProject/Test/Assets/UI_Script/StageLoader.cs
@@ -9,10 +9,19 @@ public class StageLoader : MonoBehaviour
     /// 스테이지 불러올 때 전송하는 변수, 적당히 끌어 쓰면 됨
     /// </summary>
     public static int Stage { get; private set; }
+    /// <summary>
+    /// 스테이지 불러올 때 전송하는 아이템값
+    /// </summary>
+    public static int Item { get; private set; }
 
     public void SetStageNumber(int stage)
     {
         Stage = stage;
+    }
+
+    public void SetItemID(int itemID)
+    {
+        Item = itemID;
     }
 
     public void LoadStage()
