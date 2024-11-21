9ed2d67b3f0097068f0fe0ed6f29cdcffa19fa03
GTByun <byungt02@naver.com>
2024-11-21 12:52:27 +0900

    11.21 다양한 수정사항
    1. UI 디자인 적용
    2. 시작 화면 터치 시 블러
    3. Dictionary 페이지 아이템 설명 추가
    4. 부스트 아이템 사용 안함 추가

---
+++ UnityProject/Test/Assets/UI_Script/ItemData/ItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "dictItemData", menuName = "Scriptable Object/dictItemData", order = int.MaxValue)]
+public class ItemData : ScriptableObject
+{
+    public Sprite sprite;
+    public string ItemName;
+    [Multiline(4)]
+    public string ItemDesciption;
+}
